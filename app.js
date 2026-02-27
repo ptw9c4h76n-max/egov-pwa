a263709aae018317a55148fa5681b6536ef3f104
ptw9c4h76n-max <ptw9c4h76n@privaterelay.appleid.com>
2026-02-27 18:11:04 +0600

    Update app.js

--- app.js
+++ app.js
@@ -12,26 +12,12 @@ function showRequisites() {
   document.getElementById("tabDoc").classList.remove("active");
 }
 
-/* ===============================
-   МОДАЛЬНОЕ ОКНО + QR + СВАЙП
-================================ */
-
-const modal = document.getElementById("qrModal");
-const sheet = document.getElementById("qrSheet");
-
-let startY = 0;
-let currentY = 0;
-let isDragging = false;
-let interval; // чтобы можно было остановить таймер
-
 function openAccess() {
+  const qrSection = document.getElementById("qrSection");
   const timerEl = document.getElementById("timer");
   const shortCodeEl = document.getElementById("shortCode");
 
-  modal.classList.remove("hidden");
-  setTimeout(() => {
-    modal.classList.add("active");
-  }, 10);
+  qrSection.classList.remove("hidden");
 
   const randomCode = Math.floor(100000 + Math.random() * 900000);
   shortCodeEl.innerText = randomCode;
@@ -46,64 +32,17 @@ function openAccess() {
   let time = 60;
   timerEl.innerText = "Срок действия QR-кода: 01:00";
 
-  clearInterval(interval);
-
-  interval = setInterval(() => {
+  const interval = setInterval(() => {
     time--;
     let seconds = time < 10 ? "0" + time : time;
     timerEl.innerText = "Срок действия QR-кода: 00:" + seconds;
 
     if (time <= 0) {
-      closeAccess();
+      clearInterval(interval);
+      qrSection.classList.add("hidden");
     }
   }, 1000);
 }
-
-function closeAccess() {
-  modal.classList.remove("active");
-  setTimeout(() => {
-    modal.classList.add("hidden");
-    sheet.style.transform = "translateY(100%)";
-  }, 300);
-
-  clearInterval(interval);
-}
-
-/* ===============================
-   СВАЙП ВНИЗ ДЛЯ ЗАКРЫТИЯ
-================================ */
-
-sheet.addEventListener("touchstart", (e) => {
-  startY = e.touches[0].clientY;
-  isDragging = true;
-});
-
-sheet.addEventListener("touchmove", (e) => {
-  if (!isDragging) return;
-
-  currentY = e.touches[0].clientY;
-  let diff = currentY - startY;
-
-  if (diff > 0) {
-    sheet.style.transform = `translateY(${diff}px)`;
-  }
-});
-
-sheet.addEventListener("touchend", () => {
-  isDragging = false;
-
-  let diff = currentY - startY;
-
-  if (diff > 120) {
-    closeAccess();
-  } else {
-    sheet.style.transform = "translateY(0)";
-  }
-});
-
-/* ===============================
-   SHARE
-================================ */
 
 async function shareData() {
   const text = `
