30d58da7026b17369dd9645a1507948549204c15
ptw9c4h76n-max <ptw9c4h76n@privaterelay.appleid.com>
2026-03-02 12:56:33 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,61 +1,55 @@
-let qrInterval;
-
 function showDocument() {
   document.getElementById("documentSection").classList.remove("hidden");
   document.getElementById("requisitesSection").classList.add("hidden");
-
   document.getElementById("tabDoc").classList.add("active");
   document.getElementById("tabReq").classList.remove("active");
-
-  document.getElementById("openBtn").classList.remove("hidden");
-  document.getElementById("shareBtn").classList.add("hidden");
 }
 
 function showRequisites() {
   document.getElementById("documentSection").classList.add("hidden");
   document.getElementById("requisitesSection").classList.remove("hidden");
-
   document.getElementById("tabReq").classList.add("active");
   document.getElementById("tabDoc").classList.remove("active");
-
-  document.getElementById("openBtn").classList.add("hidden");
-  document.getElementById("shareBtn").classList.remove("hidden");
 }
 
+function openAccess() {
+  const qrSection = document.getElementById("qrSection");
+  const timerEl = document.getElementById("timer");
+  const shortCodeEl = document.getElementById("shortCode");
 
+  qrSection.classList.remove("hidden");
 
-  modal.classList.remove("hidden");
-
-  const code = Math.floor(100000 + Math.random() * 900000);
-  shortCodeEl.innerText = code;
+  const randomCode = Math.floor(100000 + Math.random() * 900000);
+  shortCodeEl.innerText = randomCode;
 
   document.getElementById("qrcode").innerHTML = "";
   new QRCode(document.getElementById("qrcode"), {
-    text: code.toString(),
+    text: randomCode.toString(),
     width: 220,
     height: 220
   });
 
   let time = 60;
+  timerEl.innerText = "Срок действия QR-кода: 01:00";
 
-  qrInterval = setInterval(() => {
+  const interval = setInterval(() => {
     time--;
-    const seconds = time < 10 ? "0" + time : time;
-    timerEl.innerText = "Срок действия: 00:" + seconds;
+    let seconds = time < 10 ? "0" + time : time;
+    timerEl.innerText = "Срок действия QR-кода: 00:" + seconds;
 
     if (time <= 0) {
-      closeQR();
+      clearInterval(interval);
+      qrSection.classList.add("hidden");
     }
   }, 1000);
 }
 
-function closeQR() {
-  clearInterval(qrInterval);
-  document.getElementById("qrModal").classList.add("hidden");
-}
-
 async function shareData() {
-  const text = `ФИО: ТВОЁ ФИО`;
+  const text = `
+ФИО: ТВОЁ ФИО
+ИИН: 123456789012
+Дата рождения: 01.01.2000
+`;
 
   if (navigator.share) {
     await navigator.share({
